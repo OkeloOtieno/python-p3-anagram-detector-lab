ecc5d0ee9ef955224af3e5bcc4e024ca256d1ee5
Emmanuel <emmanuelokello294@gmail.com>
2024-05-22 16:06:03 +0300

    completes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.wordletters = sorted([letter for letter in word])
+       
+    def match(self, wordlist):
+        match_wordlist = []
+
+        for word in wordlist:
+            if sorted([letter for letter in word]) == self.wordletters:
+                match_wordlist.append(word)
+
+        return match_wordlist
